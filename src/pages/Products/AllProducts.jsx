974f569b16b20dc816935c4986061c240d3172d6
FaizulOsman <faizulosmanrabby@gmail.com>
2023-12-23 19:40:02 +0600

    feat: product data load in all products page

--- src/pages/Products/AllProducts.jsx
+++ src/pages/Products/AllProducts.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { useGetAllProductQuery } from "../../redux/features/product/productApi";
 
 const AllProducts = () => {
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const { data: getAllProducts } = useGetAllProductQuery({
+    limit,
+    page,
+    searchTerm,
+  });
+  console.log(getAllProducts);
+
   return <div>Products</div>;
 };
 
