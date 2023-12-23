12a3a4c6d3551ec7c503edbfed7a1f1a885de909
FaizulOsman <faizulosmanrabby@gmail.com>
2023-12-23 21:19:33 +0600

    update: product page update

--- src/pages/Products/ProductCard.jsx
+++ src/pages/Products/ProductCard.jsx
@@ -2,21 +2,27 @@ import React from "react";
 import HoverEffectButton from "../../components/Shared/HoverEffectButton/HoverEffectButton";
 import { Link } from "react-router-dom";
 
-const SingleProduct = ({ product }) => {
+const ProductCard = ({ product }) => {
   return (
     <div>
       <div className="max-w-sm rounded overflow-hidden hover:shadow-lg p-6 mb-6 bg-white border-2 border-gray-200">
         <div className="font-bold text-xl mb-6 flex justify-between hover:scale-110 duration-500">
           <img
             className="h-64 mx-auto "
-            src="https://i.ibb.co/drv8fjW/apple-2.png"
-            alt="img"
+            src={product?.image}
+            alt={product?.title}
           />
         </div>
         <p className="text-[#04c5ff] text-xl font-semibold">{product?.title}</p>
-        <div className="flex flex-row justify-between py-4">
-          <p className="text-gray-900 text-xl mb-2">$500</p>
-          <HoverEffectButton text="Add To Cart" />
+        <div className="flex flex-row justify-between items-center py-4">
+          <div>
+            <p className="text-gray-900 text-lg font-semibold">Variation:</p>
+            <p className="text-sm pl-4">Color: {product?.variation?.color}</p>
+            <p className="text-sm pl-4">Size: {product?.variation?.size}</p>
+          </div>
+          <div>
+            <HoverEffectButton text="Add To Cart" />
+          </div>
         </div>
         <Link to={`/products/${product?.id}`}>
           <button className="bg-orange-500 hover:bg-white text-white hover:text-orange-500 hover:border hover:border-orange-500 font-bold py-2 px-4 rounded w-full">
@@ -28,4 +34,4 @@ const SingleProduct = ({ product }) => {
   );
 };
 
-export default SingleProduct;
+export default ProductCard;
