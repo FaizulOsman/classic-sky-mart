95608153e5474fea5da8bb62037ab75c9f44304a
FaizulOsman <faizulosmanrabby@gmail.com>
2023-12-23 19:26:03 +0600

    feat: all products page created

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
+import AllProducts from "../Products/AllProducts";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <AllProducts />
+    </div>
+  );
 };
 
 export default Home;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import MainLayout from "../layouts/MainLayout";
+import AllProducts from "../pages/Products/AllProducts";
 
 const Routers = () => {
   return (
@@ -9,7 +10,7 @@ const Routers = () => {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/products" element={<AllProducts />} />
         </Route>
       </Routes>
     </BrowserRouter>
